a9a786f2a0fbb2b86b2b95334575199871e32581
Dan Whitacre <--global>
2025-05-13 20:26:01 -0400

    some updates to the post page

--- apps/hds-tm-events/src/app/post.component.ts
+++ apps/hds-tm-events/src/app/post.component.ts
@@ -8,34 +8,61 @@ import { StoreService } from 'src/services/store.service'
 @Component({
   selector: 'post',
   template: `
-    <div *ngIf="post; else noPost" class="post-container">
-      <img [src]="post.image" alt="Post Image" class="post-image" />
-      <div class="post-content">
-        <h1>{{ post.title }}</h1>
-        <p>By {{ post.author.name }} on {{ post.dateModified | date }}</p>
-        <div>{{ post.content }}</div>
+    <layout [title]="'Holy Dynasty'" [showWeeklyLeagueMenuItems]="false">
+      <div *ngIf="post; else noPost" class="post-container">
+        <img [src]="post.image" alt="Post Image" class="post-image" />
+        <div class="post-data">
+          <h1>{{ post.title }}</h1>
+          <div class="post-meta-row">
+            <player-info [player]="post.author" prefix="By "></player-info>
+            <span class="post-date">{{ post.dateModified | date: 'short' : 'UTC' }}</span>
+          </div>
+          <div class="post-content">{{ post.content }}</div>
+        </div>
       </div>
-    </div>
-    <ng-template #noPost>
-      <div class="post-container">
-        <h1>Post not found</h1>
-      </div>
-    </ng-template>
+      <ng-template #noPost>
+        <div class="post-container">
+          <h1>Post not found</h1>
+        </div>
+      </ng-template>
+    </layout>
   `,
   styles: [
     `
+      h1 {
+        margin: 12px 0;
+      }
       .post-container {
         padding: 16px;
-        background-color: #121212;
         color: #ffffff;
       }
       .post-image {
         width: 100%;
-        height: auto;
+        aspect-ratio: 16 / 9;
+        max-height: 480px;
+        object-fit: cover;
         margin-bottom: 16px;
       }
-      .post-content {
+      .post-data {
         font-size: 1.2em;
+      }
+      .post-meta-row {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 8px;
+        margin-top: -8px;
+        font-size: 0.9em;
+        color: #aaaaaa;
+      }
+      .post-date {
+        color: #aaaaaa;
+        font-size: 0.95em;
+        margin-left: 16px;
+        white-space: nowrap;
+      }
+      .post-content {
+        margin-top: 32px;
       }
     `,
   ],
