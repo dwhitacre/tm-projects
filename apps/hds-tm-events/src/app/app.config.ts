db53ca2564bebdd8e59b82a44e54a4ce35ffa294
Dan Whitacre <--global>
2025-06-17 18:28:23 -0400

    force darkmode always

--- apps/hds-tm-events/src/app/app.config.ts
+++ apps/hds-tm-events/src/app/app.config.ts
@@ -47,6 +47,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          darkModeSelector: '.hds-darkmode',
+        },
       },
     }),
     AdminService,
