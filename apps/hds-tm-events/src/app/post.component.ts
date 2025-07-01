15760865c2c330196b9e93d1bc2e41000a08bb27
Dan Whitacre <--global>
2025-07-01 18:59:59 -0400

    holy dynasty, fix up post formatting, add support for youtube embeds

--- apps/hds-tm-events/src/app/post.component.ts
+++ apps/hds-tm-events/src/app/post.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router'
 import { ComponentsModule } from 'src/components/components.module'
 import { Post } from 'src/domain/post'
 import { StoreService } from 'src/services/store.service'
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 
 @Component({
   selector: 'post',
@@ -23,15 +24,38 @@ import { StoreService } from 'src/services/store.service'
           </div>
           <div class="post-content">
             <ng-container *ngFor="let paragraph of paragraphs">
-              <p *ngIf="paragraph.startsWith('\\t'); else paragraphheader">
-                <ng-container *ngIf="paragraph.match(boldedPrefixRegex) as prefix; else normalParagraph">
-                  <b>{{ prefix[1] }}</b
-                  >{{ paragraph.slice(prefix[1].length) }}
-                </ng-container>
-                <ng-template #normalParagraph>{{ paragraph }}</ng-template>
-              </p>
-              <ng-template #paragraphheader>
+              <ng-container *ngIf="!paragraph.startsWith('\\t') && paragraph.length < 50; else paragraphcontent">
                 <h3>{{ paragraph }}</h3>
+              </ng-container>
+              <ng-template #paragraphcontent>
+                <p
+                  [ngClass]="{
+                    'post-content-indented': paragraph.startsWith('\\t'),
+                    'post-content-centered': paragraph.startsWith('https://www.youtube-nocookie.com'),
+                  }"
+                >
+                  <ng-container *ngIf="paragraph.match(boldedPrefixRegex) as prefix; else notboldedPrefix">
+                    <b>{{ prefix[1] }}</b
+                    >{{ paragraph.slice(prefix[1].length) }}
+                  </ng-container>
+                  <ng-template #notboldedPrefix>
+                    <ng-container
+                      *ngIf="paragraph.startsWith('https://www.youtube-nocookie.com'); else normalParagraph"
+                    >
+                      <iframe
+                        width="100%"
+                        height="600"
+                        [src]="getSafeUrl(paragraph)"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin"
+                        allowfullscreen
+                      ></iframe>
+                    </ng-container>
+                  </ng-template>
+                  <ng-template #normalParagraph>{{ paragraph }}</ng-template>
+                </p>
               </ng-template>
             </ng-container>
           </div>
@@ -103,11 +127,12 @@ import { StoreService } from 'src/services/store.service'
       .post-content {
         margin-top: 32px;
       }
-      .post-content h3 {
+      .post-content h3,
+      .post-content p.post-content-centered {
         justify-content: center;
         display: flex;
       }
-      .post-content p {
+      .post-content p.post-content-indented {
         text-indent: 2em;
       }
     `,
@@ -118,17 +143,22 @@ export class PostComponent {
   post?: Post
   paragraphs: string[] = []
 
-  #paragraphRegex = /\n\n/g
-  boldedPrefixRegex = /^\t?([A-Za-z0-9 ?!]+:\s?)/
+  #paragraphRegex = /\s*\n\s*\n/g
+  boldedPrefixRegex = /^\t([A-Za-z0-9]+:\s+)/
 
   constructor(
     private route: ActivatedRoute,
     public storeService: StoreService,
+    private sanitizer: DomSanitizer,
   ) {
     const postId = this.route.snapshot.paramMap.get('id')
     this.storeService.posts$.subscribe((posts) => {
       this.post = posts.find((post) => post.postId + '' === postId)
       this.paragraphs = this.post?.content?.split(this.#paragraphRegex) || []
     })
+  }
+
+  getSafeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url)
   }
 }
